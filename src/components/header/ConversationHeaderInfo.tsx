4cf428b8548781eab7bc4b5b383c00e843ecc44e
ddiu8081 <ddiu8081@gmail.com>
2023-05-04 12:19:12 +0800

    feat: add emoji icon to header

--- src/components/header/ConversationHeaderInfo.tsx
+++ src/components/header/ConversationHeaderInfo.tsx
@@ -1,3 +1,4 @@
+import { Show } from 'solid-js'
 import { useStore } from '@nanostores/solid'
 import { conversationMap, currentConversationId } from '@/stores/conversation'
 
@@ -9,13 +10,15 @@ export default () => {
   }
 
   return (
-    <div class="max-w-40vw overflow-hidden text-sm">
-      {!currentConversation() && null}
-      {currentConversation() && (
-        <div class="px-2 truncate">
+    <div class="fi gap-1 max-w-40vw px-2 overflow-hidden text-sm">
+      <Show when={currentConversation()}>
+        <Show when={currentConversation().icon}>
+          <div class="fcc w-8 h-8 rounded-full text-xl shrink-0 hidden md:flex">{currentConversation().icon}</div>
+        </Show>
+        <div class="truncate">
           {currentConversation() ? (currentConversation().name || 'Untitled') : ''}
         </div>
-      )}
+      </Show>
     </div>
   )
 }
