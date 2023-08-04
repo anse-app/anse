4f4c301d27035231b6e3a2c90ce82e8a637300a0
xiexin12138 <xiexin344@163.com>
2023-08-04 12:46:07 +0800

    删除错误添加的 defualt api key

--- src/providers/openai/index.ts
+++ src/providers/openai/index.ts
@@ -14,7 +14,6 @@ const providerOpenAI = () => {
         key: 'apiKey',
         name: 'API Key',
         type: 'api-key',
-        default: process.env.OPENAI_API_KEY || ''
       },
       {
         key: 'baseUrl',
