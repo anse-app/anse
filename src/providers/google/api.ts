5af74e5827232f005543b61d70cdd2efcea0a5c2
ddiu8081 <ddiu8081@gmail.com>
2023-12-19 03:37:00 +0800

    chore: code style

--- src/providers/google/api.ts
+++ src/providers/google/api.ts
@@ -1,15 +1,15 @@
 export interface GoogleFetchPayload {
-    apiKey: string
-    body: Record<string, any>
-    model?: string
+  apiKey: string
+  body: Record<string, any>
+  model?: string
 }
 
 export const fetchChatCompletion = async(payload: GoogleFetchPayload) => {
-    const { apiKey, body, model } = payload || {}
-    const initOptions = {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'POST',
-        body: JSON.stringify({ ...body }),
-    }
-    return fetch(`https://generativelanguage.googleapis.com/v1/models/${model}:generateContent?key=${apiKey}`, initOptions);
+  const { apiKey, body, model } = payload || {}
+  const initOptions = {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({ ...body }),
+  }
+  return fetch(`https://generativelanguage.googleapis.com/v1/models/${model}:generateContent?key=${apiKey}`, initOptions)
 }
