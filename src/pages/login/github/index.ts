8fe880a85d54bac24f3e8bff13fa42914f2912a1
李兴广 <lixingguang@xunlei.com>
2023-12-22 20:44:32 +0800

    feat: not httpOnly

--- src/pages/login/github/index.ts
+++ src/pages/login/github/index.ts
@@ -5,7 +5,7 @@ import type { APIRoute } from 'astro'
 export const get: APIRoute = async(context) => {
   const [url, state] = await githubAuth.getAuthorizationUrl()
   context.cookies.set('github_oauth_state', state, {
-    httpOnly: true,
+    // httpOnly: true,
     secure: !import.meta.env.DEV,
     path: '/',
     maxAge: 60 * 60,
