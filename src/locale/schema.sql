9da98fcb8d1d1bb6f571277b116945a9f22bf0d0
李兴广 <lixingguang@xunlei.com>
2023-12-23 15:20:48 +0800

    feat: add github auth scheme

---
+++ src/locale/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE auth_user (
+    id VARCHAR(15) PRIMARY KEY,
+    username VARCHAR(31) NOT NULL
+);
+CREATE TABLE user_key (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(15) NOT NULL,
+    hashed_password VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES auth_user(id)
+);
+CREATE TABLE user_session (
+    id VARCHAR(127) PRIMARY KEY,
+    user_id VARCHAR(15) NOT NULL,
+    active_expires BIGINT NOT NULL,
+    idle_expires BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES auth_user(id)
+);
