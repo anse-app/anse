db59988c9e28ac0794e1a47c9f07c62c2fc05fca
李兴广 <lixingguang@xunlei.com>
2023-12-26 12:24:07 +0800

    feat: review

--- src/providers/chatglm/api.ts
+++ src/providers/chatglm/api.ts
@@ -10,7 +10,7 @@ export const fetchChatCompletion = async(payload: OpenAIFetchPayload) => {
   const [id, secret] = payload.apiKey.split('.')
   const timestamp = Date.now()
   const token = await new SignJWT({
-    api_key: id, exp: timestamp + 3600 * 1000, timestamp
+    api_key: id, exp: timestamp + 3600 * 1000, timestamp,
   })
     .setProtectedHeader({ alg: 'HS256', sign_type: 'SIGN' })
     .sign(new TextEncoder().encode(secret))
