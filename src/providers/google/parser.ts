1aa84af0974a293d6aaa21529f0740b7ee011dfb
李兴广 <lixingguang@xunlei.com>
2023-12-29 11:58:13 +0800

    feat: add google chat stream support

--- src/providers/google/parser.ts
+++ src/providers/google/parser.ts
@@ -1,4 +1,6 @@
+import { createParser } from 'eventsource-parser'
 import type { Message } from '@/types/message'
+import type { ParsedEvent, ReconnectInterval } from 'eventsource-parser'
 
 export const parseMessageList = (rawList: Message[]) => {
   interface GoogleGeminiMessage {
@@ -32,4 +34,42 @@ export const parseMessageList = (rawList: Message[]) => {
     })
   }
   return parsedList
+}
+
+export const parseStream = (rawResponse: Response) => {
+  const encoder = new TextEncoder()
+  const decoder = new TextDecoder()
+  const rb = rawResponse.body as ReadableStream
+
+  return new ReadableStream({
+    async start(controller) {
+      const streamParser = (event: ParsedEvent | ReconnectInterval) => {
+        if (event.type === 'event') {
+          const data = event.data
+          try {
+            const json = JSON.parse(data)
+            const text = json.candidates[0].content.parts[0].text || ''
+            const queue = encoder.encode(text)
+            controller.enqueue(queue)
+            if (json.candidates[0].finishReason === 'STOP')
+              controller.close()
+          } catch (e) {
+            controller.error(e)
+          }
+        }
+      }
+      const reader = rb.getReader()
+      const parser = createParser(streamParser)
+      let done = false
+      while (!done) {
+        const { done: isDone, value } = await reader.read()
+        if (isDone) {
+          done = true
+          controller.close()
+          return
+        }
+        parser.feed(decoder.decode(value))
+      }
+    },
+  })
 }
