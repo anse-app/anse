d768369c0649f53c12384c2e01e43d77c7caf2fe
ddiu8081 <ddiu8081@gmail.com>
2024-02-16 22:33:46 +0800

    chore: update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import unocss from 'unocss/astro'
 import solidJs from '@astrojs/solid-js'
 import node from '@astrojs/node'
 import AstroPWA from '@vite-pwa/astro'
-import vercel from '@astrojs/vercel/edge'
-import netlify from '@astrojs/netlify/edge-functions'
+import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify/functions'
 import disableBlocks from './plugins/disableBlocks'
 
 const envAdapter = () => {
   switch (process.env.OUTPUT) {
-    case 'vercel': return vercel({ analytics: false }) // Set `analytics` to `true` if you want to use Vercel Analytics
-    case 'netlify': return netlify()
+    case 'vercel': return vercel({ edgeMiddleware: true })
+    case 'netlify': return netlify({ edgeMiddleware: true })
     default: return node({ mode: 'standalone' })
   }
 }
@@ -18,7 +18,9 @@ const envAdapter = () => {
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    unocss(),
+    unocss({
+      injectReset: true,
+    }),
     solidJs(),
     AstroPWA({
       registerType: 'autoUpdate',
