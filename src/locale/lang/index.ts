efcdd01db24e7ca63a76c4f408a581450b64c9de
Samuel Yossef <144824195+TI1A@users.noreply.github.com>
2024-03-23 17:36:59 -0300

    language  pt
    export const pt = {
      name: 'pt',
      desc: 'Português',
      locales: {
        settings: {
          title: 'Configurações',

--- src/locale/lang/index.ts
+++ src/locale/lang/index.ts
@@ -1,4 +1,5 @@
-export * from './en'
-export * from './fr'
-export * from './zh-cn'
-export * from './zh-hk'
+export * from './en';
+export * from './fr';
+export * from './zh-cn';
+export * from './zh-hk';
+export * from './pt'; // Adicionando exportação para o idioma Português
---
+++ src/locale/lang/pt.ts
@@ -0,0 +1,76 @@
+import type { language } from '..'
+
+export const pt = {
+  name: 'pt',
+  desc: 'Português',
+  locales: {
+    settings: {
+      title: 'Configurações',
+      save: 'Salvar',
+      general: {
+        title: 'Geral',
+        requestWithBackend: 'Requisitar com Backend',
+        locale: 'Alterar idioma do sistema',
+      },
+      openai: {
+        title: 'OpenAI',
+        key: '',
+      },
+      replicate: {},
+    },
+    conversations: {
+      title: 'Conversas',
+      add: 'Nova',
+      recent: 'Recentes',
+      noRecent: 'Sem recentes',
+      untitled: 'Sem título',
+      promopt: {
+        system: 'Informações do Sistema',
+        desc: 'Você é um assistente prestativo, responda da forma mais concisa possível...',
+      },
+      emoji: 'Pesquisar um emoji ~',
+      confirm: {
+        title: 'Excluir todas as mensagens nesta conversa',
+        desc: 'Esta ação excluirá todas as mensagens nesta conversa e não pode ser desfeita',
+        message: 'Excluir este registro',
+        btn: 'Confirmar',
+        cancel: 'Cancelar',
+        submit: 'Enviar',
+      },
+      share: {
+        title: 'Compartilhar Conversa',
+        link: {
+          title: 'Compartilhar com link',
+          copy: 'Copiar link',
+          create: 'Criar link',
+        },
+        save: 'Salvar',
+        copy: 'Copiar Contexto',
+        messages: {
+          title: 'Selecionar mensagem',
+          selected: 'Mensagens selecionadas',
+          selectAll: 'Selecionar tudo',
+        },
+        tabs: {
+          context: 'Compartilhar Contexto',
+          image: 'Compartilhar imagem',
+        },
+        image: {
+          btn: 'Gerar imagem',
+          open: 'Abrir em nova guia',
+          loading: 'Gerando...',
+          copy: 'Copiar imagem',
+        },
+      },
+    },
+    docs: 'Documentos',
+    github: 'Github',
+    scroll: 'Rolar até o final',
+    empty: 'Sem dados',
+    send: {
+      placeholder: 'Digite algo...',
+      button: 'Enviar',
+    },
+    copied: 'Copiado!',
+  },
+} as language
